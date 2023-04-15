6827d612d080fee900eceba71b15d2b6a8be8be4
juliojm1310 <juliojm1310@gmail.com>
2023-04-15 13:11:00 +0300

    Fix code style

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-
-from blog.user.views import user
 from blog.articles.views import articles
+from blog.user.views import user
+
+from flask import Flask
 
 
 def create_app() -> Flask:
